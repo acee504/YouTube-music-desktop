afe03a15252a7c31b581fa860f255933c6ded2d3
acee504 <acee504@gmail.com>
2024-08-27 11:37:05 +0200

    main commit

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
-const { app, BrowserWindow, Menu } = require("electron");
-const isMac = process.platform === "darwin"
+const isMac = process.platform === "darwin";
+const electron = require("electron");
+const { app, BrowserWindow, Menu } = electron;
 
-function createMainWindow(){
-    const mainWindow = new BrowserWindow({
-        title: "YouTube Music",
-        width: 1500,
-        height: 900,
-        webPreferences: {
-            devTools: false
-            },
-        icon:__dirname + '/yt.ico'
-    });
-
-    mainWindow.loadURL("https://music.youtube.com/") //loads either the html index (with loadfile) or a website with loadURL
+function createMainWindow() {
+  const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
+  const mainWindow = new BrowserWindow({
+    title: "YouTube Music",
+    width,
+    height,
+    webPreferences: {
+      devTools: false,
+    },
+    icon: __dirname + "/yt.ico",
+  });
+  mainWindow.maximize();
+  mainWindow.loadURL("https://music.youtube.com/"); //loads either the html index (with loadfile) or a website with loadURL
 }
 
 app.whenReady().then(() => {
-    createMainWindow();
-    const mainMenu = Menu.buildFromTemplate(menu);
-    Menu.setApplicationMenu(mainMenu);
-    app.on("activate", ()=>{
-        if(BrowserWindow.getAllWindows().length===0){
-            createMainWindow();
-        }
-    })
+  createMainWindow();
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
 });
 
 //Menu template
-const menu=[]
+const menu = [];
 
-app.on("window-all-closed", ()=>{
-    if(!isMac){
-        app.quit()
-    }
-})
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
